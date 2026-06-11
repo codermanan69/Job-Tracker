e2c75615560d7fd1a08e04ea4f0cae615437ad56
codermanan69 <manansahni295@gmail.com>
2026-06-12 01:31:28 +0530

    dist

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 const authMiddleware = require("./middleware/auth.middleware");
 const applicationRoutes = require("./Routes/application.routes");
 const authRoutes = require("./Routes/auth.routes");
+
 
 const app = express();
 app.use(express.json());
@@ -13,13 +15,17 @@ app.use("/api/applications", applicationRoutes);
 
 app.use("/api/auth", authRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Job Tracker API Running");
-});
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/protected", authMiddleware, (req, res) => {
   res.json({
     message: "Protected Route Accessed",
     user: req.user,
   });
+});
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "public", "index.html")
+  );
 });
 module.exports = app;
--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-hot-toast";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "/api",
 });
 
 API.interceptors.request.use((req) => {
